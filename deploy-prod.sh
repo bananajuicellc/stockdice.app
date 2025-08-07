18b95ea8cbad67d9b770147550374b4e2e2fbde5
Tim Swast <tswast@gmail.com>
2025-08-06 22:25:42 -0500

    more tries at cloud run

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-uv export --format requirements-txt --locked --no-editable --no-hashes > requirements.txt
+uv export --format requirements-txt --locked --no-hashes > requirements.txt
 
 gcloud run deploy stockdiceapp --source . --project stockdice-app --region us-central1
